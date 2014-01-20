bd4f3f5d3b29a917262fde6c5b1ec63dfe014f27
Manuel Riezebosch <mriezebosch@gmail.com>
2014-01-20 16:27:33 +0100

    Entity Framework demo klaar met IQueryable

---
+++ EntityFrameworkDemo/EntityFrameworkDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+
+    class MyContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyContext())
+            {
+                var query = from p in context.People
+                            where p.Name.StartsWith("m")
+                            select p.Name;
+
+                foreach (var p in query)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+    }
+}
