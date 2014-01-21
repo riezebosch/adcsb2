b4ce1d840cf5d9a767a09e80dea2d4a3d64f0d60
Manuel Riezebosch <mriezebosch@gmail.com>
2014-01-21 13:52:13 +0100

    Awaitable en Process & Cancellation

---
+++ AwaitableDemo/AwaitableDemo/ProcessExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwaitableDemo
+{
+    static class ProcessExtensions
+    {
+        public static TaskAwaiter<int> GetAwaiter(this Process p)
+        {
+            var tcs = new TaskCompletionSource<int>();
+            
+            p.EnableRaisingEvents = true;
+            p.Exited += (o, i) => tcs.TrySetResult(p.ExitCode);
+
+            if (p.HasExited)
+            {
+                tcs.TrySetResult(p.ExitCode);
+            }
+
+            return tcs.Task.GetAwaiter();
+        }
+    }
+}
---
+++ ProgressAndCancellationDemo/ProgressAndCancellationDemo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressAndCancellationDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        CancellationTokenSource token;
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var progress = new Progress<int>(i => progressBar1.Value = i);
+            token = new CancellationTokenSource();
+
+            try
+            {
+                await Run(progress, token.Token);
+                button1.Text = "Finished!";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private static Task Run(IProgress<int> progress, CancellationToken token)
+        {
+            return Task.Run(() =>
+                {
+                    for (int i = 0; i <= 100; i++)
+                    {
+                        token.ThrowIfCancellationRequested();
+
+                        progress.Report(i);
+                        Thread.Sleep(100);
+                    }
+                });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                token.Cancel();
+            
+        }
+    }
+}
